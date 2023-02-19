e4aa7da09995e57a4444563d2b1951b4059fb525
Daiki <miwa.daiki.mllab.nit@gmail.com>
2023-02-19 13:40:41 +0900

    add key mapping of jupynium

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -7,3 +7,11 @@ vim.cmd [[
       nmap <buffer> dd <Plug>(fern-action-cd)
 endfunction
 ]]
+
+vim.keymap.set({ "n", "x" }, "<space>x", "<cmd>JupyniumExecuteSelectedCells<CR>", { buffer = buf_id })
+vim.keymap.set({ "n", "x" }, "<space>c", "<cmd>JupyniumClearSelectedCellsOutputs<CR>", { buffer = buf_id })
+vim.keymap.set({ "n", "x" }, "<space>S", "<cmd>JupyniumScrollToCell<cr>", { buffer = buf_id })
+vim.keymap.set({ "n", "x" }, "<space>T", "<cmd>JupyniumToggleSelectedCellsOutputsScroll<cr>", { buffer = buf_id })
+vim.keymap.set({ "n", "x" }, "<space>R", "<cmd>JupyniumStartSync<cr>", { buffer = buf_id })
+vim.keymap.set("", "<PageUp>", "<cmd>JupyniumScrollUp<cr>", { buffer = buf_id })
+vim.keymap.set("", "<PageDown>", "<cmd>JupyniumScrollDown<cr>", { buffer = buf_id })

63f625acfefb561eeb726b37e44b3be266b6c30f
DaikiMiwa <miwa.daiki.mllab.nit@gmail.com>
2023-10-08 13:43:06 +0900

    change rename key mapping

--- .config/nvim/lua/plugins/lspsaga.lua
+++ .config/nvim/lua/plugins/lspsaga.lua
@@ -6,7 +6,7 @@ return {
         vim.keymap.set("n","gd","<cmd>Lspsaga goto_definition<cr>",{noremap = true, silent = true})
         vim.keymap.set("n","gp","<cmd>Lspsaga peek_definition<cr>",{noremap = true, silent = true})
         vim.keymap.set("n","K","<cmd>Lspsaga hover_doc<cr>",{noremap = true, silent = true})
-        vim.keymap.set("n","ln","<cmd>Lspsaga rename<cr>",{noremap = true, silent = true})
+        vim.keymap.set("n","<leader>rn","<cmd>Lspsaga rename<cr>",{noremap = true, silent = true})
     end,
     dependencies = {
         'nvim-treesitter/nvim-treesitter', -- optional
